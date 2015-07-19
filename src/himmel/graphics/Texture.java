d66dd199760a1d81b7a35de73e116e2c6693cb3e
IgorBoyarshin <igorb314@gmail.com>
2015-07-19 17:15:28 +0300

    Constructor with reduced amount of parameters and some cleanup

--- src/himmel/graphics/Texture.java
+++ src/himmel/graphics/Texture.java
@@ -21,18 +21,14 @@ public class Texture {
 
     public static final int FILTER_NEAREST = 0;
     public static final int FILTER_LINEAR = 1;
-//    private final int TYPE;
 
-    public Texture(String path, final int type, final int minFilter, final int magFilter) {
-//        TYPE = type;
-
-        textureID = loadTexture(path, type, minFilter, magFilter);
-//        this.TYPE = TYPE_RGB;
+    public Texture(String path, final int type) {
+        textureID = loadTexture(path, type);
     }
 
-//    public final int getType() {
-//        return TYPE;
-//    }
+    public Texture(String path, final int type, final int minFilter, final int magFilter) {
+        textureID = loadTexture(path, type, minFilter, magFilter);
+    }
 
     public void bind() {
         glBindTexture(GL_TEXTURE_2D, textureID);
@@ -44,6 +40,10 @@ public class Texture {
 
     public int getTID() {
         return textureID;
+    }
+
+    private int loadTexture(String path, int type) {
+        return loadTexture(path, type, FILTER_NEAREST, FILTER_LINEAR);
     }
 
     private int loadTexture(String path, int type, int minFilter, int magFilter) {
@@ -79,19 +79,13 @@ public class Texture {
 
         int id = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, id);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter == FILTER_LINEAR ? GL_LINEAR : GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter == FILTER_LINEAR ? GL_LINEAR : GL_NEAREST);
 
-//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, i.getWidth(), i.getHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageBuffer);
         glTexImage2D(GL_TEXTURE_2D, 0, type == TYPE_RGB ? GL_RGB : GL_RGBA,
                 i.getWidth(), i.getHeight(), 0, type == TYPE_RGB ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, imageBuffer);
-//        if (TYPE == TYPE_RGB) {
-//            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, i.getWidth(), i.getHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageBuffer);
-//        } else {
-//            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, i.getWidth(), i.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, imageBuffer);
-//        }
+
         glBindTexture(GL_TEXTURE_2D, 0);
 
         return id;
