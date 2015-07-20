17a803f5c630c56925c6f6ad7398b7b077e2b4bb
IgorBoyarshin <igorb314@gmail.com>
2015-07-20 12:21:14 +0300

    Vector3i class added

---
+++ src/himmel/math/Vector3i.java
@@ -0,0 +1,20 @@
+package himmel.math;
+
+/**
+ * Created by Igor on 20-Jul-15.
+ */
+public class Vector3i {
+    public int x, y, z;
+
+    public Vector3i() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Vector3i(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
