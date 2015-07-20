f95ea921cb22534f44031108028ffc882379ece2
IgorBoyarshin <igorb314@gmail.com>
2015-07-20 12:19:40 +0300

    Vector2i class added

---
+++ src/himmel/math/Vector2i.java
@@ -0,0 +1,18 @@
+package himmel.math;
+
+/**
+ * Created by Igor on 20-Jul-15.
+ */
+public class Vector2i {
+    public int x, y;
+
+    public Vector2i() {
+        x = 0;
+        y = 0;
+    }
+
+    public Vector2i(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
