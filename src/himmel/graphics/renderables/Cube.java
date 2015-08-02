271597b3cb32ed3e1cab52d07c0909d86abb249c
Igor Boyarshin <igorb314@gmail.com>
2015-08-02 17:55:49 +0300

    Cube and Rectangle classes added. To be implemented

---
+++ src/himmel/graphics/renderables/Cube.java
@@ -0,0 +1,7 @@
+package himmel.graphics.renderables;
+
+/**
+ * Created by Igor on 02-Aug-15.
+ */
+public class Cube extends Renderable {
+}
---
+++ src/himmel/graphics/renderables/Rectangle.java
@@ -0,0 +1,10 @@
+package himmel.graphics.renderables;
+
+import himmel.graphics.renderers.RenderingSet;
+
+/**
+ * Created by Igor on 02-Aug-15.
+ */
+public class Rectangle extends Renderable {
+
+}
