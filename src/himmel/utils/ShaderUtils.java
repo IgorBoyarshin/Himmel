3c897e859d956c66bd3be115f5cc9cb615164528
Igor Boyarshin <igorb314@gmail.com>
2016-02-04 23:06:46 +0200

    ShaderUtils class structure simplified, less redundant now

--- src/himmel/utils/ShaderUtils.java
+++ src/himmel/utils/ShaderUtils.java
@@ -1,5 +1,7 @@
 package himmel.utils;
 
+import himmel.log.Log;
+
 import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL20.glDeleteShader;
@@ -11,217 +13,120 @@ import static org.lwjgl.opengl.GL40.GL_TESS_EVALUATION_SHADER;
  * Created by Igor on 01-May-15.
  */
 public class ShaderUtils {
-    public static int load(String vertPath, String fragPath) {
-        String vert = FileLoader.loadAsString(vertPath);
-        String frag = FileLoader.loadAsString(fragPath);
 
-        return create(vert, frag);
-    }
-
-    public static int load(String vertPath, String geomPath, String fragPath) {
-        String vert = FileLoader.loadAsString(vertPath);
-        String geom = FileLoader.loadAsString(geomPath);
-        String frag = FileLoader.loadAsString(fragPath);
-
-        return create(vert, geom, frag);
-    }
-
-    public static int load(String vertPath, String tessControlPath, String tessEvaluationPath, String fragPath) {
-        String vert = FileLoader.loadAsString(vertPath);
-        String tcs = FileLoader.loadAsString(tessControlPath);
-        String tes = FileLoader.loadAsString(tessEvaluationPath);
-        String frag = FileLoader.loadAsString(fragPath);
-
-        return create(vert, tcs, tes, frag);
-    }
-
-    public static int load(String vertPath, String tessControlPath, String tessEvaluationPath, String geomPath, String fragPath) {
-        String vert = FileLoader.loadAsString(vertPath);
-        String tcs = FileLoader.loadAsString(tessControlPath);
-        String tes = FileLoader.loadAsString(tessEvaluationPath);
-        String geom = FileLoader.loadAsString(geomPath);
-        String frag = FileLoader.loadAsString(fragPath);
-
-        return create(vert, tcs, tes, geom, frag);
-    }
-
-    public static int create(String vert, String tcs, String tes, String frag) {
-        int program = glCreateProgram();
-        int vertID = glCreateShader(GL_VERTEX_SHADER);
-        int tcsId = glCreateShader(GL_TESS_CONTROL_SHADER);
-        int tesId = glCreateShader(GL_TESS_EVALUATION_SHADER);
-        int fragID = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(vertID, vert);
-        glShaderSource(tcsId, tcs);
-        glShaderSource(tesId, tes);
-        glShaderSource(fragID, frag);
-
-        glCompileShader(vertID);
-        if (glGetShaderi(vertID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Vertex Shader");
-            System.err.println(glGetShaderInfoLog(vertID, 2048));
+    public static int loadShaderId(String[] paths) {
+        String[] shadersAsStrings = new String[paths.length];
+        for (int i = 0; i < paths.length; i++) {
+            shadersAsStrings[i] = FileLoader.loadAsString(paths[i]);
         }
 
-        glCompileShader(tcsId);
-        if (glGetShaderi(tcsId, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Tessellation Control Shader");
-            System.err.println(glGetShaderInfoLog(tcsId, 2048));
+        return createShaderProgram(shadersAsStrings);
+    }
+
+    private static int createShaderProgram(String[] shaders) {
+        final int program = glCreateProgram();
+        int[] shadersIds = new int[shaders.length];
+
+        for (int i = 0; i < shaders.length; i++) {
+            ShaderType shaderType = ShaderType.getShaderTypeFromIndex(shaders.length, i);
+            final int shaderId = glCreateShader(shaderType.code);
+
+            glShaderSource(shaderId, shaders[i]);
+            glCompileShader(shaderId);
+            if (glGetShaderi(shaderId, GL_COMPILE_STATUS) == GL_FALSE) {
+                Log.logError("<ShaderUtils.createShaderProgram>: Failed to compile " + shaderType.name + " shader.");
+                Log.logError(glGetShaderInfoLog(shaderId, 2048));
+            }
+            glAttachShader(program, shaderId);
+
+            shadersIds[i] = shaderId;
         }
 
-        glCompileShader(tesId);
-        if (glGetShaderi(tesId, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Tessellation Evaluation Shader");
-            System.err.println(glGetShaderInfoLog(tesId, 2048));
-        }
-
-        glCompileShader(fragID);
-        if (glGetShaderi(fragID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Fragment Shader");
-            System.err.println(glGetShaderInfoLog(fragID, 2048));
-        }
-
-        glAttachShader(program, vertID);
-        glAttachShader(program, tcsId);
-        glAttachShader(program, tesId);
-        glAttachShader(program, fragID);
         glLinkProgram(program);
         glValidateProgram(program);
 
-        glDeleteShader(fragID);
-        glDeleteShader(tesId);
-        glDeleteShader(tcsId);
-        glDeleteShader(vertID);
+        for (int i = 0; i < shaders.length; i++) {
+            glDeleteShader(shadersIds[i]);
+        }
 
         return program;
     }
 
-    public static int create(String vert, String tcs, String tes, String geom, String frag) {
-        int program = glCreateProgram();
-        int vertID = glCreateShader(GL_VERTEX_SHADER);
-        int tcsId = glCreateShader(GL_TESS_CONTROL_SHADER);
-        int tesId = glCreateShader(GL_TESS_EVALUATION_SHADER);
-        int geomId = glCreateShader(GL_GEOMETRY_SHADER);
-        int fragID = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(vertID, vert);
-        glShaderSource(tcsId, tcs);
-        glShaderSource(tesId, tes);
-        glShaderSource(geomId, geom);
-        glShaderSource(fragID, frag);
+    private enum ShaderType {
+        VERTEX("Vertex", GL_VERTEX_SHADER),
+        TESSELLATION_CONTROL("Tessellation Control", GL_TESS_CONTROL_SHADER),
+        TESSELLATION_EVALUATION("Tessellation Evaluation", GL_TESS_EVALUATION_SHADER),
+        GEOMETRY("Geometry", GL_GEOMETRY_SHADER),
+        FRAGMENT("Fragment", GL_FRAGMENT_SHADER);
 
-        glCompileShader(vertID);
-        if (glGetShaderi(vertID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Vertex Shader");
-            System.err.println(glGetShaderInfoLog(vertID, 2048));
+        public final String name;
+        public final int code;
+
+        ShaderType(String name, final int code) {
+            this.name = name;
+            this.code = code;
         }
 
-        glCompileShader(tcsId);
-        if (glGetShaderi(tcsId, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Tessellation Control Shader");
-            System.err.println(glGetShaderInfoLog(tcsId, 2048));
+        public static ShaderType getShaderTypeFromIndex(int size, int id) {
+            switch (size) {
+                case 2:
+                    switch (id) {
+                        case 0:
+                            return VERTEX;
+                        case 1:
+                            return FRAGMENT;
+                        default:
+                            Log.logError("<ShaderUtils.ShaderType.getShaderTypeFromIndex>: wrong id argument(" + id + ").");
+                    }
+                    break;
+                case 3:
+                    switch (id) {
+                        case 0:
+                            return VERTEX;
+                        case 1:
+                            return GEOMETRY;
+                        case 2:
+                            return FRAGMENT;
+                        default:
+                            Log.logError("<ShaderUtils.ShaderType.getShaderTypeFromIndex>: wrong id argument(" + id + ").");
+                    }
+                    break;
+                case 4:
+                    switch (id) {
+                        case 0:
+                            return VERTEX;
+                        case 1:
+                            return TESSELLATION_CONTROL;
+                        case 2:
+                            return TESSELLATION_EVALUATION;
+                        case 3:
+                            return FRAGMENT;
+                        default:
+                            Log.logError("<ShaderUtils.ShaderType.getShaderTypeFromIndex>: wrong id argument(" + id + ").");
+                    }
+                    break;
+                case 5:
+                    switch (id) {
+                        case 0:
+                            return VERTEX;
+                        case 1:
+                            return TESSELLATION_CONTROL;
+                        case 2:
+                            return TESSELLATION_EVALUATION;
+                        case 3:
+                            return GEOMETRY;
+                        case 4:
+                            return FRAGMENT;
+                        default:
+                            Log.logError("<ShaderUtils.ShaderType.getShaderTypeFromIndex>: wrong id argument(" + id + ").");
+                    }
+                    break;
+                default:
+                    Log.logError("<ShaderUtils.ShaderType.getShaderTypeFromIndex>: wrong size argument(" + size + ").");
+                    return null;
+            }
+
+            return null;
         }
-
-        glCompileShader(tesId);
-        if (glGetShaderi(tesId, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Tessellation Evaluation Shader");
-            System.err.println(glGetShaderInfoLog(tesId, 2048));
-        }
-
-        glCompileShader(geomId);
-        if (glGetShaderi(geomId, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Geometry Shader");
-            System.err.println(glGetShaderInfoLog(geomId, 2048));
-        }
-
-        glCompileShader(fragID);
-        if (glGetShaderi(fragID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Fragment Shader");
-            System.err.println(glGetShaderInfoLog(fragID, 2048));
-        }
-
-        glAttachShader(program, vertID);
-        glAttachShader(program, tcsId);
-        glAttachShader(program, tesId);
-        glAttachShader(program, geomId);
-        glAttachShader(program, fragID);
-        glLinkProgram(program);
-        glValidateProgram(program);
-
-        glDeleteShader(fragID);
-        glDeleteShader(geomId);
-        glDeleteShader(tesId);
-        glDeleteShader(tcsId);
-        glDeleteShader(vertID);
-
-        return program;
-    }
-
-    public static int create(String vert, String geom, String frag) {
-        int program = glCreateProgram();
-        int vertID = glCreateShader(GL_VERTEX_SHADER);
-        int geomId = glCreateShader(GL_GEOMETRY_SHADER);
-        int fragID = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(vertID, vert);
-        glShaderSource(geomId, geom);
-        glShaderSource(fragID, frag);
-
-        glCompileShader(vertID);
-        if (glGetShaderi(vertID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Vertex Shader");
-            System.err.println(glGetShaderInfoLog(vertID, 2048));
-        }
-
-        glCompileShader(geomId);
-        if (glGetShaderi(geomId, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Geometry Shader");
-            System.err.println(glGetShaderInfoLog(geomId, 2048));
-        }
-
-        glCompileShader(fragID);
-        if (glGetShaderi(fragID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Fragment Shader");
-            System.err.println(glGetShaderInfoLog(fragID, 2048));
-        }
-
-        glAttachShader(program, vertID);
-        glAttachShader(program, geomId);
-        glAttachShader(program, fragID);
-        glLinkProgram(program);
-        glValidateProgram(program);
-
-        glDeleteShader(fragID);
-        glDeleteShader(geomId);
-        glDeleteShader(vertID);
-
-        return program;
-    }
-
-    public static int create(String vert, String frag) {
-        int program = glCreateProgram();
-        int vertID = glCreateShader(GL_VERTEX_SHADER);
-        int fragID = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(vertID, vert);
-        glShaderSource(fragID, frag);
-
-        glCompileShader(vertID);
-        if (glGetShaderi(vertID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Vertex Shader");
-            System.err.println(glGetShaderInfoLog(vertID, 2048));
-        }
-
-        glCompileShader(fragID);
-        if (glGetShaderi(fragID, GL_COMPILE_STATUS) == GL_FALSE) {
-            System.err.println("Failed to compile Fragment Shader");
-            System.err.println(glGetShaderInfoLog(fragID, 2048));
-        }
-
-        glAttachShader(program, vertID);
-        glAttachShader(program, fragID);
-        glLinkProgram(program);
-        glValidateProgram(program);
-
-        glDeleteShader(fragID);
-        glDeleteShader(vertID);
-
-        return program;
     }
 }
