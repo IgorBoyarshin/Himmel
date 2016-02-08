8820340a9e3a1c7ce6bfae0ad2da33c58cc7bb98
Igor Boyarshin <igorb314@gmail.com>
2016-02-08 22:33:30 +0200

    FloatArray class now has another new constructor

--- src/himmel/utils/FloatArray.java
+++ src/himmel/utils/FloatArray.java
@@ -2,7 +2,7 @@ package himmel.utils;
 
 /**
  * Created by Igor on 03-Feb-16 at 9:04 PM.
- *
+ * <p>
  * Class provides an easy way to store a float array.
  */
 public class FloatArray {
@@ -10,6 +10,13 @@ public class FloatArray {
 
     public FloatArray(float elements[]) {
         this.elements = elements;
+    }
+
+    public FloatArray(float element, int count) {
+        this.elements = new float[count];
+        for (int i = 0; i < count; i++) {
+            elements[i] = element;
+        }
     }
 
     public int length() {
