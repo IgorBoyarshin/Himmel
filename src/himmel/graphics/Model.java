0be50224acb19b70476cf5d00275f60a482b1e7b
Igor Boyarshin <igorb314@gmail.com>
2016-02-08 16:49:20 +0200

    Model class rewritten. Now supports uvs, either short[] or int[] indices and works 3 times faster!

--- src/himmel/graphics/Model.java
+++ src/himmel/graphics/Model.java
@@ -1,7 +1,8 @@
 package himmel.graphics;
 
+import himmel.math.Vector2f;
 import himmel.math.Vector3f;
-import himmel.math.Vector3s;
+import himmel.math.Vector3i;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -15,188 +16,206 @@ import java.util.List;
 public class Model {
     private float[] vertices;
     private float[] normals;
-    private short[] indices;
+    private float[] uvs;
+    private int[] indicesInts;
+    private short[] indicesShorts;
+    private boolean indicesAsShorts;
+
+    private final int MAX_SHORT_VALUE = Short.MAX_VALUE;
 
     public Model(String pathToModel) {
-        try {
-            loadModel(pathToModel);
+        loadModel(pathToModel);
+    }
+
+    private void loadModel(String path) {
+        long startLoading = System.currentTimeMillis();
+
+        // Load data
+
+        List<Vector3f> positionsArray = new ArrayList<>();
+        List<Vector2f> uvsArray = new ArrayList<>();
+        List<Vector3f> normalsArray = new ArrayList<>();
+        List<Face> facesArray = new ArrayList<>();
+        boolean uvsPresent = false;
+        boolean normalsPresent = false;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("v ")) {
+                    String[] values = line.split(" ");
+
+                    final float x = Float.valueOf(values[1]);
+                    final float y = Float.valueOf(values[2]);
+                    final float z = Float.valueOf(values[3]);
+
+                    positionsArray.add(new Vector3f(x, y, z));
+                } else if (line.startsWith("vt ")) {
+                    String[] values = line.split(" ");
+
+                    final float x = Float.valueOf(values[1]);
+                    final float y = Float.valueOf(values[2]);
+
+                    uvsArray.add(new Vector2f(x, y));
+                    uvsPresent = true;
+                } else if (line.startsWith("vn ")) {
+                    String[] values = line.split(" ");
+
+                    final float x = Float.valueOf(values[1]);
+                    final float y = Float.valueOf(values[2]);
+                    final float z = Float.valueOf(values[3]);
+
+                    normalsArray.add(new Vector3f(x, y, z));
+                    normalsPresent = true;
+                } else if (line.startsWith("f ")) {
+                    String[] groups = line.split(" ");
+                    String[] vertex0 = groups[1].split("/");
+                    String[] vertex1 = groups[2].split("/");
+                    String[] vertex2 = groups[3].split("/");
+
+                    // -1 because .obj format starts with 1, not 0
+                    facesArray.add(new Face(
+                            new Vector3i(
+                                    Integer.valueOf(emptyCheck(vertex0[0])) - 1,
+                                    Integer.valueOf(emptyCheck(vertex0[1])) - 1,
+                                    Integer.valueOf(emptyCheck(vertex0[2])) - 1),
+                            new Vector3i(
+                                    Integer.valueOf(emptyCheck(vertex1[0])) - 1,
+                                    Integer.valueOf(emptyCheck(vertex1[1])) - 1,
+                                    Integer.valueOf(emptyCheck(vertex1[2])) - 1),
+                            new Vector3i(
+                                    Integer.valueOf(emptyCheck(vertex2[0])) - 1,
+                                    Integer.valueOf(emptyCheck(vertex2[1])) - 1,
+                                    Integer.valueOf(emptyCheck(vertex2[2])) - 1)
+                    ));
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
-    private void loadModel(String path) throws IOException {
-        List<Vector3f> verticesArray = new ArrayList<>();
-        List<Vector3f> normalsArray = new ArrayList<>();
-        List<Face> facesArray = new ArrayList<>();
-
-        // Load data
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("v ")) {
-                float x = Float.valueOf(line.split(" ")[1]);
-                float y = Float.valueOf(line.split(" ")[2]);
-                float z = Float.valueOf(line.split(" ")[3]);
-                verticesArray.add(new Vector3f(x, y, z));
-            } else if (line.startsWith("vn ")) {
-                float x = Float.valueOf(line.split(" ")[1]);
-                float y = Float.valueOf(line.split(" ")[2]);
-                float z = Float.valueOf(line.split(" ")[3]);
-                normalsArray.add(new Vector3f(x, y, z));
-            } else if (line.startsWith("f ")) {
-                Vector3s vertexIndex = new Vector3s(
-                        (short) (Short.valueOf(line.split(" ")[1].split("/")[0]) - 1),
-                        (short) (Short.valueOf(line.split(" ")[2].split("/")[0]) - 1),
-                        (short) (Short.valueOf(line.split(" ")[3].split("/")[0]) - 1));
-                Vector3s normalIndex = new Vector3s(
-                        (short) (Short.valueOf(line.split(" ")[1].split("/")[2]) - 1),
-                        (short) (Short.valueOf(line.split(" ")[2].split("/")[2]) - 1),
-                        (short) (Short.valueOf(line.split(" ")[3].split("/")[2]) - 1));
-                facesArray.add(new Face(vertexIndex, normalIndex));
-            }
-        }
-
+        long finishLoad = System.currentTimeMillis();
         // Process data
-        List<Vertex> vertices = new ArrayList<>();
-        List<Short> indices = new ArrayList<>();
-        for (short face = 0; face < facesArray.size(); face++) {
-            Vertex vertex1 = new Vertex(verticesArray.get(facesArray.get(face).vertices.x),
-                    normalsArray.get(facesArray.get(face).normals.x));
-            Vertex vertex2 = new Vertex(verticesArray.get(facesArray.get(face).vertices.y),
-                    normalsArray.get(facesArray.get(face).normals.y));
-            Vertex vertex3 = new Vertex(verticesArray.get(facesArray.get(face).vertices.z),
-                    normalsArray.get(facesArray.get(face).normals.z));
 
-            boolean matchFound = false;
+        List<Vector3i> verticesAsIndices = new ArrayList<>(); // as <v,vt,vn> triples
+        List<Integer> indices = new ArrayList<>();
 
-            // V1
-            for (short i = 0; i < vertices.size(); i++) {
-                if (vertices.get(i).equals(vertex1)) {
-                    matchFound = true;
-                    indices.add(i);
-                    break;
-                }
-            }
-            if (!matchFound) {
-                vertices.add(vertex1);
-                indices.add((short) (vertices.size() - 1));
-            }
-            matchFound = false;
-
-            // V2
-            for (short i = 0; i < vertices.size(); i++) {
-                if (vertices.get(i).equals(vertex2)) {
-                    matchFound = true;
-                    indices.add(i);
-                    break;
-                }
-            }
-            if (!matchFound) {
-                vertices.add(vertex2);
-                indices.add((short) (vertices.size() - 1));
-            }
-            matchFound = false;
-
-            // V3
-            for (short i = 0; i < vertices.size(); i++) {
-                if (vertices.get(i).equals(vertex3)) {
-                    matchFound = true;
-                    indices.add(i);
-                    break;
-                }
-            }
-            if (!matchFound) {
-                vertices.add(vertex3);
-                indices.add((short) (vertices.size() - 1));
+        for (Face face : facesArray) {
+            for (Vector3i vertex : face.verticesIndices) {
+                indices.add(addElementAndReturnIndex(vertex, verticesAsIndices));
             }
         }
 
+        long finishProcess = System.currentTimeMillis();
         // Return data
-        this.vertices = new float[vertices.size() * 3];
-        this.normals = new float[vertices.size() * 3];
-        this.indices = new short[indices.size()];
 
-        for (int i = 0; i < vertices.size(); i++) {
-            Vertex vertex = vertices.get(i);
-            this.vertices[3 * i + 0] = vertex.position.x;
-            this.vertices[3 * i + 1] = vertex.position.y;
-            this.vertices[3 * i + 2] = vertex.position.z;
-
-            this.normals[3 * i + 0] = vertex.normal.x;
-            this.normals[3 * i + 1] = vertex.normal.y;
-            this.normals[3 * i + 2] = vertex.normal.z;
+        indicesAsShorts = (verticesAsIndices.size() <= MAX_SHORT_VALUE);
+        this.vertices = new float[verticesAsIndices.size() * 3];
+        this.uvs = (uvsPresent) ? (new float[verticesAsIndices.size() * 2]) : null;
+        this.normals = (normalsPresent) ? (new float[verticesAsIndices.size() * 3]) : null;
+        if (indicesAsShorts) {
+            this.indicesShorts = new short[indices.size()];
+        } else {
+            this.indicesInts = new int[indices.size()];
         }
 
-        for (int i = 0; i < indices.size(); i++) {
-            this.indices[i] = indices.get(i);
+        for (int i = 0; i < verticesAsIndices.size(); i++) {
+            Vector3i vertexIndices = verticesAsIndices.get(i);
+
+            Vector3f position = positionsArray.get(vertexIndices.x);
+            Vector2f uv = (uvsPresent) ? uvsArray.get(vertexIndices.y) : null;
+            Vector3f normal = (normalsPresent) ? normalsArray.get(vertexIndices.z) : null;
+
+            this.vertices[3 * i + 0] = position.x;
+            this.vertices[3 * i + 1] = position.y;
+            this.vertices[3 * i + 2] = position.z;
+
+            if (uvsPresent) {
+                this.uvs[2 * i + 0] = uv.x;
+                this.uvs[2 * i + 1] = uv.y;
+            }
+
+            if (normalsPresent) {
+                this.normals[3 * i + 0] = normal.x;
+                this.normals[3 * i + 1] = normal.y;
+                this.normals[3 * i + 2] = normal.z;
+            }
         }
 
-//        System.out.println("verts");
-//        int counter = 0;
-//        for (float f : this.vertices) {
-//            System.out.print(f + " ");
-//            counter++;
-//            if (counter == 3) {
-//                counter = 0;
-//                System.out.println();
-//            }
-//        }
-//        System.out.println("ind");
-//        counter = 0;
-//        for (short f : this.indices) {
-//            System.out.print(f + " ");
-//        }
+        if (indicesAsShorts) {
+            for (int i = 0; i < indices.size(); i++) {
+                this.indicesShorts[i] = indices.get(i).shortValue();
+            }
+        } else {
+            for (int i = 0; i < indices.size(); i++) {
+                this.indicesInts[i] = indices.get(i);
+            }
+        }
+
+
+        long endLoading = System.currentTimeMillis();
+//        System.out.println("Load time: " + (finishLoad - startLoading));
+//        System.out.println("Process time: " + (finishProcess - finishLoad));
+//        System.out.println("Return time: " + (endLoading - finishProcess));
+//        System.out.println("Total loading time: " + (endLoading - startLoading));
     }
 
-    public short[] getIndices() {
-        return indices;
+    private String emptyCheck(String string) {
+        final String safetyString = "1";
+        return (string.length() == 0) ? (safetyString) : (string);
     }
 
-    public float[] getNormals() {
-        return normals;
+    /**
+     * Adds the vertex if the array does not contain such int triple(not object!!).
+     */
+    private int addElementAndReturnIndex(Vector3i vertex, List<Vector3i> array) {
+        for (int elementIndex = 0; elementIndex < array.size(); elementIndex++) {
+            if (vectorsEqual(vertex, array.get(elementIndex))) {
+                return elementIndex;
+            }
+        }
+
+        array.add(vertex);
+        return (array.size() - 1);
+    }
+
+    private boolean vectorsEqual(Vector3i v1, Vector3i v2) {
+        return (v1.x == v2.x &&
+                v1.y == v2.y &&
+                v1.z == v2.z);
     }
 
     public float[] getVertices() {
         return vertices;
     }
 
-    private class Vertex {
-        public Vector3f position;
-        public Vector3f normal;
+    public float[] getNormals() {
+        return normals;
+    }
 
-        public Vertex(Vector3f position, Vector3f normal) {
-            this.position = position;
-            this.normal = normal;
-        }
+    public float[] getUvs() {
+        return uvs;
+    }
 
-        public boolean equals(Vertex otherVertex) {
-            if (this.position.x == otherVertex.position.x &&
-                    this.position.y == otherVertex.position.y &&
-                    this.position.z == otherVertex.position.z) {
-                if (this.normal.x == otherVertex.normal.x &&
-                        this.normal.y == otherVertex.normal.y &&
-                        this.normal.z == otherVertex.normal.z) {
-                    return true;
-                }
-            }
+    public int[] getIndicesInts() {
+        return indicesInts;
+    }
 
-            return false;
-        }
+    public short[] getIndicesShorts() {
+        return indicesShorts;
+    }
+
+    public boolean areIndicesAsShorts() {
+        return indicesAsShorts;
     }
 
     private class Face {
-        public Vector3s vertices;
-        public Vector3s normals;
+        public Vector3i[] verticesIndices = new Vector3i[3]; // <v, vt, vn> 3 times
 
-        public Face(Vector3s vertices, Vector3s normals) {
-            this.vertices = vertices;
-            this.normals = normals;
+        public Face(Vector3i v1, Vector3i v2, Vector3i v3) {
+            verticesIndices[0] = v1;
+            verticesIndices[1] = v2;
+            verticesIndices[2] = v3;
         }
-
-//        public List<Vertex> getArray() {
-//            List<Vertex> list = new ArrayList<>();
-//            list.add(new Vertex(vertices.x));
-//        }
     }
 }
